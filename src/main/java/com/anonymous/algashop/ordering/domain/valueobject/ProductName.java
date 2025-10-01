2ed4b12bf8f594d11e408073717e8ec2e2ef831b
Carlos Eduardo Guerra Resende <cadu.guerra@gmail.com>
2025-10-01 07:54:34 -0300

    Implementing some Value Objects

---
+++ src/main/java/com/anonymous/algashop/ordering/domain/valueobject/ProductName.java
@@ -0,0 +1,15 @@
+package com.anonymous.algashop.ordering.domain.valueobject;
+
+import com.anonymous.algashop.ordering.domain.validator.FieldValidations;
+
+public record ProductName(String value) {
+
+    public ProductName {
+        FieldValidations.requiresNonBlank(value);
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
+}

ba01949ab0803c9e6fbcf8cec68176c01e043c31
Carlos Eduardo Guerra Resende <cadu.guerra@gmail.com>
2025-10-02 11:00:36 -0300

    Adding OrderItem to an Order

--- src/main/java/com/anonymous/algashop/ordering/domain/entity/Order.java
+++ src/main/java/com/anonymous/algashop/ordering/domain/entity/Order.java
@@ -1,11 +1,9 @@
 package com.anonymous.algashop.ordering.domain.entity;
 
-import com.anonymous.algashop.ordering.domain.valueobject.BillingInfo;
-import com.anonymous.algashop.ordering.domain.valueobject.Money;
-import com.anonymous.algashop.ordering.domain.valueobject.Quantity;
-import com.anonymous.algashop.ordering.domain.valueobject.ShippingInfo;
+import com.anonymous.algashop.ordering.domain.valueobject.*;
 import com.anonymous.algashop.ordering.domain.valueobject.id.CustomerId;
 import com.anonymous.algashop.ordering.domain.valueobject.id.OrderId;
+import com.anonymous.algashop.ordering.domain.valueobject.id.ProductId;
 import lombok.Builder;
 
 import java.time.LocalDate;
@@ -79,6 +77,23 @@ public class Order {
             null,
             new HashSet<>()
         );
+    }
+
+    public void addItem(ProductId productId, ProductName productName,
+                        Money price, Quantity quantity) {
+        OrderItem orderItem = OrderItem.brandNew()
+                .orderId(this.id())
+                .price(price)
+                .quantity(quantity)
+                .productId(productId)
+                .productName(productName)
+                .build();
+
+        if(this.items == null) {
+            this.items = new HashSet<>();
+        }
+
+        this.items.add(orderItem);
     }
 
     public OrderId id() {
--- src/test/java/com/anonymous/algashop/ordering/domain/entity/OrderTest.java
+++ src/test/java/com/anonymous/algashop/ordering/domain/entity/OrderTest.java
@@ -1,14 +1,45 @@
 package com.anonymous.algashop.ordering.domain.entity;
 
 
+import com.anonymous.algashop.ordering.domain.valueobject.Money;
+import com.anonymous.algashop.ordering.domain.valueobject.ProductName;
+import com.anonymous.algashop.ordering.domain.valueobject.Quantity;
 import com.anonymous.algashop.ordering.domain.valueobject.id.CustomerId;
+import com.anonymous.algashop.ordering.domain.valueobject.id.ProductId;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class OrderTest {
 
     @Test
-    public void shouldGenerate() {
+    void shouldGenerate() {
         Order order = Order.draft(new CustomerId());
+    }
+
+    @Test
+    void shouldAddItem() {
+        Order order = Order.draft(new CustomerId());
+        ProductId productId = new ProductId();
+
+        order.addItem(
+                productId,
+                new ProductName("Mouse pad"),
+                new Money("100"),
+                new Quantity(1)
+        );
+
+        Assertions.assertThat(order.items()).hasSize(1);
+
+        OrderItem orderItem = order.items().iterator().next();
+
+        Assertions.assertWith(orderItem,
+                (i) -> Assertions.assertThat(i.id()).isNotNull(),
+                (i) -> Assertions.assertThat(i.productName()).isEqualTo(new ProductName("Mouse pad")),
+                (i) -> Assertions.assertThat(i.productId()).isEqualTo(productId),
+                (i) -> Assertions.assertThat(i.price()).isEqualTo(new Money("100")),
+                (i) -> Assertions.assertThat(i.quantity()).isEqualTo(new Quantity(1))
+                );
+
     }
 
 }

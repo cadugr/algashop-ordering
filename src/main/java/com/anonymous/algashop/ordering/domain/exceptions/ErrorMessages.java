2a7dedbca77519502b79a3521894d3268cab0cc3
Carlos Eduardo Guerra Resende <cadu.guerra@gmail.com>
2025-10-05 11:07:54 -0300

    Upgrading Exceptions with Factory Method

--- src/main/java/com/anonymous/algashop/ordering/domain/exceptions/ErrorMessages.java
+++ src/main/java/com/anonymous/algashop/ordering/domain/exceptions/ErrorMessages.java
@@ -12,6 +12,22 @@ public class ErrorMessages {
     public static final String ERROR_ORDER_DELIVERY_DATE_CANNOT_BE_IN_THE_PAST =
             "Order %s expected delivery date cannot be in the past";
 
-    public static final String ERROR_ORDER_CANNOT_BE_PLACED_HAS_NOT_ITEMS
+    public static final String ERROR_ORDER_CANNOT_BE_PLACED_HAS_NO_ITEMS
             = "Order %s cannot be placed, it has no items";
+
+    public static final String ERROR_ORDER_CANNOT_BE_PLACED_HAS_NO_SHIPPING_INFO
+            = "Order %s cannot be placed, it has no shipping info";
+
+    public static final String ERROR_ORDER_CANNOT_BE_PLACED_HAS_NO_BILLING_INFO
+            = "Order %s cannot be placed, it has no billing info";
+
+    public static final String ERROR_ORDER_CANNOT_BE_PLACED_INVALID_SHIPPING_COST
+            = "Order %s cannot be placed, it has invalid shipping cost";
+
+    public static final String ERROR_ORDER_CANNOT_BE_PLACED_INVALID_DELIVERY_DATE
+            = "Order %s cannot be placed, it has invalid delivery date";
+
+    public static final String ERROR_ORDER_CANNOT_BE_PLACED_HAS_NO_PAYMENT_METHOD
+            = "Order %s cannot be placed, it has no payment method";
+
 }

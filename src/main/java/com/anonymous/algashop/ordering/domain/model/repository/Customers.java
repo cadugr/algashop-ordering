fa582e355a7bcba030db611f6569b68b2097a2d2
Carlos Eduardo Guerra Resende <cadu.guerra@gmail.com>
2025-12-11 19:23:20 -0300

    Defining a repository on domain model

---
+++ src/main/java/com/anonymous/algashop/ordering/domain/model/repository/Customers.java
@@ -0,0 +1,7 @@
+package com.anonymous.algashop.ordering.domain.model.repository;
+
+import com.anonymous.algashop.ordering.domain.model.entity.Customer;
+import com.anonymous.algashop.ordering.domain.model.valueobject.id.CustomerId;
+
+public interface Customers extends Repository<Customer, CustomerId> {
+}
---
+++ src/main/java/com/anonymous/algashop/ordering/domain/model/repository/Orders.java
@@ -0,0 +1,7 @@
+package com.anonymous.algashop.ordering.domain.model.repository;
+
+import com.anonymous.algashop.ordering.domain.model.entity.Order;
+import com.anonymous.algashop.ordering.domain.model.valueobject.id.OrderId;
+
+public interface Orders extends Repository<Order, OrderId> {
+}

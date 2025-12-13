b6387c01db72bd3f62cffbcdcd006204321e8901
Carlos Eduardo Guerra Resende <cadu.guerra@gmail.com>
2025-12-13 09:26:28 -0300

    Optimizing persistence tests with @DataJpaTest

--- src/test/java/com/anonymous/algashop/ordering/infrastructure/persistence/repository/OrderPersistenceEntityRepositoryIT.java
+++ src/test/java/com/anonymous/algashop/ordering/infrastructure/persistence/repository/OrderPersistenceEntityRepositoryIT.java
@@ -5,14 +5,16 @@ import com.anonymous.algashop.ordering.infrastructure.persistence.entity.OrderPe
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigDecimal;
 import java.time.OffsetDateTime;
 
-@SpringBootTest
-@Transactional
+@DataJpaTest //This annotation is used to test JPA repositories. It configures in-memory database, Hibernate, Spring Data, and other relevant components.
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE) // This annotation is used to control the configuration of the test database. By setting replace = AutoConfigureTestDatabase.Replace.NONE, it indicates that the test should use the actual database configuration defined in the application instead of replacing it with an in-memory database.
 class OrderPersistenceEntityRepositoryIT {
 
     private final OrderPersistenceEntityRepository orderPersistenceEntityRepository;

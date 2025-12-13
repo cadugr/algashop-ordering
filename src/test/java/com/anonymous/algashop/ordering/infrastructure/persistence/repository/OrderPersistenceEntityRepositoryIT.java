0e62e9862e2acc9fcffbe3a1ac46fb0dd22e8cd9
Carlos Eduardo Guerra Resende <cadu.guerra@gmail.com>
2025-12-13 16:49:23 -0300

    Implementing unique properties to persistence model

--- src/test/java/com/anonymous/algashop/ordering/infrastructure/persistence/repository/OrderPersistenceEntityRepositoryIT.java
+++ src/test/java/com/anonymous/algashop/ordering/infrastructure/persistence/repository/OrderPersistenceEntityRepositoryIT.java
@@ -1,6 +1,6 @@
 package com.anonymous.algashop.ordering.infrastructure.persistence.repository;
 
-import com.anonymous.algashop.ordering.domain.model.utility.IdGenerator;
+import com.anonymous.algashop.ordering.infrastructure.persistence.config.SpringDataAuditingConfig;
 import com.anonymous.algashop.ordering.infrastructure.persistence.entity.OrderPersistenceEntity;
 import com.anonymous.algashop.ordering.infrastructure.persistence.entity.OrderPersistenceEntityTestDataBuilder;
 import org.assertj.core.api.Assertions;
@@ -8,14 +8,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.transaction.annotation.Transactional;
-
-import java.math.BigDecimal;
-import java.time.OffsetDateTime;
+import org.springframework.context.annotation.Import;
 
 @DataJpaTest //This annotation is used to test JPA repositories. It configures in-memory database, Hibernate, Spring Data, and other relevant components.
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE) // This annotation is used to control the configuration of the test database. By setting replace = AutoConfigureTestDatabase.Replace.NONE, it indicates that the test should use the actual database configuration defined in the application instead of replacing it with an in-memory database.
+@Import(SpringDataAuditingConfig.class)
 class OrderPersistenceEntityRepositoryIT {
 
     private final OrderPersistenceEntityRepository orderPersistenceEntityRepository;
@@ -37,6 +34,16 @@ class OrderPersistenceEntityRepositoryIT {
     void shouldCount() {
         long ordersCount = orderPersistenceEntityRepository.count();
         Assertions.assertThat(ordersCount).isZero();
+    }
+
+    @Test
+    void shouldSetAuditingValues() {
+        OrderPersistenceEntity entity = OrderPersistenceEntityTestDataBuilder.existingOrder().build();
+        entity = orderPersistenceEntityRepository.saveAndFlush(entity);
+
+        Assertions.assertThat(entity.getCreatedByUserId()).isNotNull();
+        Assertions.assertThat(entity.getLastModifiedAt()).isNotNull();
+        Assertions.assertThat(entity.getLastModifiedByUserId()).isNotNull();
     }
 
 }

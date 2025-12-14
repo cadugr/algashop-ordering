417e0f977edefd0ba601a0eab683483eb4342357
Carlos Eduardo Guerra Resende <cadu.guerra@gmail.com>
2025-12-14 17:20:38 -0300

    Exercise: reconstructing compose value objects

--- src/main/java/com/anonymous/algashop/ordering/infrastructure/persistence/disassembler/OrderPersistenceEntityDisassembler.java
+++ src/main/java/com/anonymous/algashop/ordering/infrastructure/persistence/disassembler/OrderPersistenceEntityDisassembler.java
@@ -3,10 +3,13 @@ package com.anonymous.algashop.ordering.infrastructure.persistence.disassembler;
 import com.anonymous.algashop.ordering.domain.model.entity.Order;
 import com.anonymous.algashop.ordering.domain.model.entity.OrderStatus;
 import com.anonymous.algashop.ordering.domain.model.entity.PaymentMethod;
-import com.anonymous.algashop.ordering.domain.model.valueobject.Money;
-import com.anonymous.algashop.ordering.domain.model.valueobject.Quantity;
+import com.anonymous.algashop.ordering.domain.model.valueobject.*;
 import com.anonymous.algashop.ordering.domain.model.valueobject.id.CustomerId;
 import com.anonymous.algashop.ordering.domain.model.valueobject.id.OrderId;
+import com.anonymous.algashop.ordering.infrastructure.persistence.embeddable.AddressEmbeddable;
+import com.anonymous.algashop.ordering.infrastructure.persistence.embeddable.BillingEmbeddable;
+import com.anonymous.algashop.ordering.infrastructure.persistence.embeddable.RecipientEmbeddable;
+import com.anonymous.algashop.ordering.infrastructure.persistence.embeddable.ShippingEmbeddable;
 import com.anonymous.algashop.ordering.infrastructure.persistence.entity.OrderPersistenceEntity;
 import org.springframework.stereotype.Component;
 
@@ -29,6 +32,45 @@ public class OrderPersistenceEntityDisassembler {
                 .readyAt(persistenceEntity.getReadyAt())
                 .items(new HashSet<>())
                 .version(persistenceEntity.getVersion())
+                .billing(toBillingValueObject(persistenceEntity.getBilling()))
+                .shipping(toShippingValueObject(persistenceEntity.getShipping()))
+                .build();
+    }
+
+    private Shipping toShippingValueObject(ShippingEmbeddable shippingEmbeddable) {
+        RecipientEmbeddable recipientEmbeddable = shippingEmbeddable.getRecipient();
+        return Shipping.builder()
+                .cost(new Money(shippingEmbeddable.getCost()))
+                .expectedDate(shippingEmbeddable.getExpectedDate())
+                .recipient(
+                        Recipient.builder()
+                                .fullName(new FullName(recipientEmbeddable.getFirstName(), recipientEmbeddable.getLastName()))
+                                .document(new Document(recipientEmbeddable.getDocument()))
+                                .phone(new Phone(recipientEmbeddable.getPhone()))
+                                .build()
+                )
+                .address(toAddressValueObject(shippingEmbeddable.getAddress()))
+                .build();
+    }
+
+    private Billing toBillingValueObject(BillingEmbeddable billingEmbeddable) {
+        return Billing.builder()
+                .fullName(new FullName(billingEmbeddable.getFirstName(), billingEmbeddable.getLastName()))
+                .document(new Document(billingEmbeddable.getDocument()))
+                .phone(new Phone(billingEmbeddable.getPhone()))
+                .address(toAddressValueObject(billingEmbeddable.getAddress()))
+                .build();
+    }
+
+    private Address toAddressValueObject(AddressEmbeddable address) {
+        return Address.builder()
+                .street(address.getStreet())
+                .number(address.getNumber())
+                .complement(address.getComplement())
+                .neighborhood(address.getNeighborhood())
+                .city(address.getCity())
+                .state(address.getState())
+                .zipCode(new ZipCode(address.getZipCode()))
                 .build();
     }
 }
